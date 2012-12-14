1eed476bec10f97c371823be252988935d48625a
Alby Reid <alby@bleary-id.co.uk>
2012-12-14 20:06:45 +0000

    Created placeholder files

---
+++ edition/index.php
@@ -0,0 +1,15 @@
+﻿<?php
+	$date = date(w);
+	$md5 = md5($date);
+	header("ETag: ".$md5);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Constellatio</title>
+		<style>
+		</style>
+	</head>
+	<body>
+	</body>
+</html>
