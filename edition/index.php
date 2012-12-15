ef4cf5323e451c1922cfc358d5f50aec6677880c
Alby Reid <alby@bleary-id.co.uk>
2012-12-15 15:42:23 +0000

    Inserted PHP variables and created styles

--- edition/index.php
+++ edition/index.php
@@ -1,13 +1,27 @@
 ﻿<?php
+	// Create eTag
 	$date = date(w);
-	$md5 = md5($date);
-	header("ETag: ".$md5);
+	$eTag = md5($date);
+	header("ETag: ".$eTag);
+	// Define variables
+	$constellation = "Cassiopeia";
+	$imageURL = "cassiopeia.png";
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Constellatio</title>
 		<style>
+			body {
+            			margin: 0;
+            			padding: 0;
+            			border: 0;
+            			font-size: 20px;
+            			font-family:'St Ryde';
+			}
+			.page {
+				width: 384px;
+			}
 		</style>
 	</head>
 	<body>
